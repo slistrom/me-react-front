5513e03fb5501aa7bb08766d665f5fb6a9c8b072
Stefan <stefan@listrom.se>
2020-09-29 09:52:21 +0200

    Testing Travis och Selenium

--- test/mocha_test.js
+++ test/mocha_test.js
@@ -26,29 +26,22 @@ function matchUrl(target) {
     });
 }
 
-function assertH1(target) {
-    browser.findElement(By.css("h1")).then(function(element) {
-        element.getText().then(function(text) {
-            assert.equal(text, target);
-        });
-    });
-}
-
-
 
 // Test suite
 test.describe("Me-page", function() {
 
-    this.timeout(0);
+    // this.timeout(20000);
 
     beforeEach(function(done) {
+        // this.timeout(20000);
         browser = new webdriver.Builder()
             .withCapabilities(webdriver.Capabilities.firefox())
             .setFirefoxOptions(new firefox.Options().headless())
             .forBrowser('firefox')
             .build();
 
-        browser.get("https://listrom.me/");
+        // browser.get("https://listrom.me/");
+        browser.get("http://localhost:3000/");
         done();
     });
 
@@ -60,18 +53,10 @@ test.describe("Me-page", function() {
 
     // Test case
     test.it("Test index", function(done) {
-        let promise = browser.getTitle();
-
-        promise.then(function(title) {
-            assert.equal(title, "My me-page jsramverk");
-        });
 
         browser.getTitle().then(function(title) {
             assert.equal(title, "My me-page jsramverk");
         });
-
-        // assertH1("Home");
-        // matchUrl("#!/");
 
         done();
     });
@@ -82,7 +67,6 @@ test.describe("Me-page", function() {
         // try use nav link
         goToNavLink("Reports");
 
-        // assertH1("Home");
         matchUrl("reports/" );
 
         done();
@@ -93,78 +77,9 @@ test.describe("Me-page", function() {
     test.it("Test go to Login", function(done) {
         goToNavLink("Login");
 
-        // get h1 text
-        // assertH1("About");
         matchUrl("login/");
 
         done();
     });
 
-
-
-    // test.it("Test go to Calculator", function(done) {
-    //     goToNavLink("Calculator");
-    //
-    //     // get h1 text
-    //     assertH1("Calculator");
-    //     matchUrl("#!/calculator");
-    //
-    //     done();
-    // });
-
-
-
-    // test.it("Test color on Calculator", function(done) {
-    //     goToNavLink("Calculator");
-    //
-    //     // display element background color
-    //     browser.findElement(By.id("display")).then(function(displayElement) {
-    //         displayElement.getCssValue("background-color").then(function(bgColor) {
-    //             assert.equal(bgColor, "rgb(221, 221, 221)");
-    //         });
-    //     });
-    //
-    //     // operator buttons background color
-    //     browser.findElements(By.className("operator")).then(function(operatorElements) {
-    //         webdriver.promise.map(operatorElements, function(element) {
-    //             return element.getCssValue('background-color');
-    //         }).then(function(colors) {
-    //             colors.forEach(function(color) {
-    //                 assert.equal(color, "rgb(0, 31, 63)");
-    //             });
-    //         });
-    //     });
-    //
-    //     done();
-    // });
-
-
-    //
-    // test.it("Test an addition calculation", function(done) {
-    //     goToNavLink("Calculator");
-    //
-    //     let promiseNumbers = browser.findElements(By.className("number"));
-    //
-    //     promiseNumbers.then(function(numberElements) {
-    //         // press number 1
-    //         numberElements[6].click();
-    //         // press +
-    //         browser.findElements(By.className("operator")).then(function(operatorElements) {
-    //             operatorElements[3].click();
-    //             // press number 5
-    //             numberElements[4].click();
-    //             // press =
-    //             operatorElements[4].click();
-    //         });
-    //     });
-    //
-    //     // check sum
-    //     browser.findElement(By.id("display")).then(function(displayElement) {
-    //         displayElement.getText().then(function(value) {
-    //             assert.equal(value, "6");
-    //         });
-    //     });
-    //
-    //     done();
-    // });
 });
